95c82f25bd3fd8977e79c89119fdbc7cadc66be0
biscuit <1224297248@qq.com>
2023-04-21 13:38:55 +0800

    first

--- bianyuandian.py
+++ bianyuandian.py
@@ -1,23 +1,20 @@
 import cv2
 
+# 读取图像并转为灰度图像
+img = cv2.imread('image.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# 读入图像
-img = cv2.imread('image.jpg', cv2.IMREAD_GRAYSCALE)
+# 边缘检测
+edges = cv2.Canny(gray, 100, 200)
 
-# 获取图像高度和宽度
-height, width = img.shape[:2]
+# 二值化处理
+ret, binary = cv2.threshold(edges, 127, 255, cv2.THRESH_BINARY)
 
-# 统计边缘像素中值为255的像素数量
-edge_pixels = 0
-for i in range(height):
-    for j in range(width):
-        if i == 0 or j == 0 or i == height-1 or j == width-1:
-            if img[i][j] == 255:
-                edge_pixels += 1
+# 统计像素点数量
+edge_pixel_count = cv2.countNonZero(binary)
+total_pixel_count = binary.shape[0] * binary.shape[1]
 
-# 计算边缘点（已置255）的数量占图像总像素数的比例
-total_pixels = height * width
-edge_pixels_ratio = edge_pixels / total_pixels
+# 计算边缘点占比
+edge_ratio = edge_pixel_count / total_pixel_count
 
-print("边缘点（已置255）的数量占图像总像素数的比例为:", edge_pixels_ratio)
-
+print("边缘点占比: {:.2%}".format(edge_ratio))
